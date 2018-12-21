13bafddf934fe02d9e6098423e7cf5fa148aa96d
Daniel Depablos <ddepabloss@gmail.com>
2018-12-21 16:24:20 -0400

    #01 Introducción

---
+++ src/01.js
@@ -0,0 +1,11 @@
+const app = new Vue({
+    el : '#app',
+    data : {
+        titulo: 'Dando el todo por el todo!!',
+        frutas: [
+            {nombre:'Pera', cantidad:12},
+            {nombre:'Manzana', cantidad:0},
+            {nombre:'Cambur', cantidad:10}
+        ]
+    }
+})
